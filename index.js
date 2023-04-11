74f41cc0b24ef8b0e797779c89360cbf45fdc033
RawiriHohepa <54158903+RawiriHohepa@users.noreply.github.com>
2023-04-11 22:17:07 +1200

    Fetch and parse json

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ require("dotenv").config();
 
 console.log(process.env.BEARER_TOKEN);
 console.log(process.env.USER_ID);
+
+(async () => {
+  const response = await fetch(
+    "https://gist.githubusercontent.com/sunilshenoy/23a3e7132c27d62599ba741bce13056a/raw/517b07fc382c843dcc7d444046d959a318695245/sample_json.json"
+  );
+  const jsonData = await response.json();
+  console.log(jsonData);
+})();
